fc09b5401926dafab65e9a025321087f6a825cdb
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-02-12 18:36:57 +0530

    Migration to 2.6

--- review.py
+++ review.py
@@ -4,33 +4,44 @@
 
     Nereid Reviews
 
-    :copyright: (c) 2012 by Openlabs Technologies & Consulting (P) Limited
+    :copyright: (c) 2012-2013 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
 from trytond.model import ModelSQL, ModelView, fields
 
+__all__ = ['NereidReview']
+
 
 class NereidReview(ModelSQL, ModelView):
-    """
-    Nereid Review
-    """
-    _name = "nereid.review"
-    _description = __doc__
+    "Nereid Review"
+    __name__ = "nereid.review"
     _rec_name = 'title'
 
     title = fields.Char('Title')
-    rating = fields.Integer('Rating(On a scale of 1-5)')
+    rating = fields.Integer('Rating (On a scale of 1-5)')
     comment = fields.Text('Comment')
-    nereid_user = fields.Many2One('nereid.user', 'Nereid User')
-    party = fields.Function(fields.Many2One(
-        'party.party', 'Party'
-    ), 'get_party')
 
-    def get_party(self, ids, name):
-        res = {}
-        for review in self.browse(ids):
-            if review.nereid_user:
-                res[review.id] = review.nereid_user.party.id
-        return res
+    # Who wrote the review
+    nereid_user = fields.Many2One(
+        'nereid.user', 'Nereid User', required=True
+    )
+    party = fields.Function(
+        fields.Many2One('party.party', 'Party'), 'get_party'
+    )
 
-NereidReview()
+    @classmethod
+    def __setup__(cls):
+        super(NereidReview, cls).__setup__()
+        cls._sql_constraints += [
+             ('valid_rating', 'CHECK (rating BETWEEN 0 AND 5)', 'invalid_rating')
+        ]
+        cls._error_messages = {
+            'invalid_rating': 'The rating has to be on a scale of 1 to 5'
+        }
+
+
+    def get_party(self, name):
+        """
+        Get the party from the nereid user
+        """
+        return self.nereid_user and self.nereid_user.party.id or None
